e7321a3de377ac79288ba1126bb1a082953943e7
Daniel Max <danielmaxsiegel@gmail.com>
2019-09-03 21:30:50 -0700

    import httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PoetryReaderComponent } from './poetry-reader/poetry-reader.component';
@@ -12,7 +13,8 @@ import { PoetryFormComponent } from './poetry-form/poetry-form.component';
     PoetryFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
